60a2e93ce839c7e39df9ebd61a8355c5abb51793
Gouda <gouda.clark@gmail.com>
2018-10-29 18:50:10 -0400

    Edit post working in state

--- client/src/components/SinglePost.js
+++ client/src/components/SinglePost.js
@@ -21,17 +21,47 @@ export default class SinglePost extends Component {
         return response.data
     }
 
+    handleChange = (event) => {
+      const post = {...this.state.post}
+      post[event.target.name] = event.target.value
+      this.setState({post})
+    }
+    handleUpdate = async (event) => {
+      event.preventDefault()
+      const cityId = this.props.match.params.cityId
+      const postId = this.props.match.params.id
+      const response = await axios.put(`/api/cities/${cityId}/posts/${postId}`, this.state.post)
+      const post = this.state.post
+      // post.push(response.data)
+      this.setState({post})
+    }
+
+
 
   render() {
-    //   const city = this.state.city
-    //   const postInfo = this.state.posts
-         
+    
           
           
       
     return (
       <div>
-        Hello from single SinglePost
+        <form onSubmit={this.handleUpdate}>
+        TITLE:
+        <input 
+          type='text'
+          name='title'
+          value={this.state.post.title}
+          onChange={this.handleChange}
+        />
+        <br/>
+        BODY:
+        <textarea 
+          name='body'
+          value={this.state.post.body}
+          onChange={this.handleChange}
+        />
+        <input type='submit' value='Edit Post'/>
+        </form>
       </div>
     )
   }
