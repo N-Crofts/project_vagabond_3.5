29be61c9800c86e3cfd5e7b1266ff331ac60012e
Gouda <gouda.clark@gmail.com>
2018-10-30 09:52:16 -0400

    seeded posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,22 +15,22 @@ atlanta = City.create(
 )
 
 post_one = atlanta.posts.create(
-    title: "Post One",
+    title: "Piedmont Park",
     date: "10/28/18",
-    body: "Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die.",
-    photo_url: "https://fillmurray.com/200/200"
+    body: "This is the largest park in the city and homes to festivals nearly every weekend during the warmer months.  Make sure to check out events such as Music Midtown, the Dogwood Festival, and the Atlanta Jazz Festival",
+    photo_url: "https://i.imgur.com/nRqWT5A.jpg"
 )
 
 post_two = atlanta.posts.create(
-    title: "Post Two",
-    date: "10/28/18",
-    body: "Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die.",
-    photo_url: "https://fillmurray.com/200/200"
+    title: "Fox Theatre",
+    date: "12/18/18",
+    body: "The Fox was a great venue for a Holiday Gucci Mane show.  Hope he comes back next year!",
+    photo_url: "https://i.imgur.com/yfgBBh9.jpg"
 )
 
-post_three = atlanta.posts.create(
-    title: "Post Three",
-    date: "10/28/18",
-    body: "Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die. Lorem ipsum crunk till you you die.",
-    photo_url: "https://fillmurray.com/200/200"
+post_three = atlanta.posts.create( 
+    title: "Starlight Six Drive In",
+    date: "11/28/17",
+    body: "Best deal in the city!  I got to watch 3 movies for 8 bucks, and I got to stay in my car.  Popcorn was super cheap too!  I see why this place has been open since 1949.",
+    photo_url: "https://i.imgur.com/3hyzDcb.jpg"
 )
